f06c601f26fc392897b5f1ba094d4d3c01806857
CSLiArr <lucalombard@hotmail.com>
2026-09-09 21:34:11 +0100

    data preprocessing script

--- src/preprocess.py
+++ src/preprocess.py
@@ -61,17 +61,20 @@ duplicated_team_ids = df_teams["team_id"].duplicated().sum()
 valid_team_ids = df_teams["team_id"]
 
 
-print(f"Duplicated Ticket IDs sum:  {duplicated_ticket_ids}")
-print(f"Duplicated Team IDs sum:  {duplicated_team_ids}")
-print(f"\nMissing ticket values {df_tickets.isna().sum()}")
-print(f"\n Missing Team values: {df_teams.isna().sum()}")
-print(f"\nTicket statuses: {df_tickets["status"].value_counts()}")
-print(f"\nTicket Priorities: {df_tickets["priority"].value_counts()}")
-print(f"\n Negative response times: {(df_tickets["response_hours"] < 0).sum()}")
-print(f"\n Negative resolution times: {(df_tickets["resolution_hours"] < 0).sum()}")
-print(f"\n invalid satisfaction scores: {len((df_tickets["satisfaction_score"] < 1) | (df_tickets["satisfaction_score"] > 5))}")
-print(f"\n Tickets with invalid team ids: {len(df_tickets[~df_tickets["team_id"].isin(valid_team_ids)])}")
+# print(f"Duplicated Ticket IDs sum:  {duplicated_ticket_ids}")
+# print(f"Duplicated Team IDs sum:  {duplicated_team_ids}")
+# print(f"\nMissing ticket values {df_tickets.isna().sum()}")
+# print(f"\n Missing Team values: {df_teams.isna().sum()}")
+# print(f"\nTicket statuses: {df_tickets["status"].value_counts()}")
+# print(f"\nTicket Priorities: {df_tickets["priority"].value_counts()}")
+# print(f"\n Negative response times: {(df_tickets["response_hours"] < 0).sum()}")
+# print(f"\n Negative resolution times: {(df_tickets["resolution_hours"] < 0).sum()}")
+# print(f"\n invalid satisfaction scores: {len((df_tickets["satisfaction_score"] < 1) | (df_tickets["satisfaction_score"] > 5))}")
+# print(f"\n Tickets with invalid team ids: {len(df_tickets[~df_tickets["team_id"].isin(valid_team_ids)])}")
 
 
 df_tickets.to_csv("data/processed/clean_tickets.csv", index=False)
 df_teams.to_csv("data/processed/teams_clean.csv", index=False)
+
+print(df_teams.head())
+
