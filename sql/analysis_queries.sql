faf64d246ae8c496dd6f173f3bfb762aed7af5b3
CSLiArr <lucalombard@hotmail.com>
2026-09-17 22:40:27 +0100

    feat: additional queries added to analysis_queries.sql

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -25,3 +25,40 @@ FROM tickets
 JOIN teams
     ON tickets.team_id = teams.team_id
 LIMIT 10;
+
+SELECT
+    teams.team_name
+    COUNT(tickets.ticket_id) AS total_tickets,
+
+    ROUND(
+        AVG(tickets.response_hours),
+        2
+    ) AS average_response_hours,
+
+    ROUND(
+        100.0 * AVG(
+            CASE
+                WHEN tickets.response_hours
+                    <= teams.target_response_hours
+                THEN 1
+                ELSE 0
+            END
+        ),
+        1
+    ) AS response_sla_percentage,
+
+    ROUND(
+        AVG(tickets.satisfaction_score),
+        2
+    )  AS average_satisfaction
+
+FROM tickets
+JOIN teams
+    ON tickets.team_id = teams.team_id
+
+GROUP BY
+    teams.team_id
+    teams.team_name
+
+ORDER BY
+    response_sla_percentage ASC;
